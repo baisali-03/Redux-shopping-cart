76385f3c8b34b2deb0eb86c48db6176db86ad150
Baisali-akrabhi <baisali.pradhan@infoviz.co>
2023-03-01 23:04:22 +0530

    added routing to the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from "./container/Header";
+import ProductListing from './container/ProductListing';
+import ProductDetail from './container/ProductDetail';
 function App() {
   return (
     <div className="App">
-      Hello
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" exact component={ProductDetail} />
+          <Route>404 Not Found !!</Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/container/ProductListing.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductListing = () => {
+  return (
+    <div><h1>ProductListing</h1></div>
+  )
+}
+
+export default ProductListing
